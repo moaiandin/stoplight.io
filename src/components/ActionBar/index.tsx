92be4e4bd4c1c58171472468957c5875b0e56f17
Sarah Spear <sarahrva@gmail.com>
2019-08-13 16:14:55 -0500

    chore: adjust styling on actionbar

--- src/components/ActionBar/index.tsx
+++ src/components/ActionBar/index.tsx
@@ -2,11 +2,9 @@ import cn from 'classnames';
 import * as React from 'react';
 import { withSiteData } from 'react-static';
 
-import { Button, IButton } from 'src/components/Button';
 import { Section } from 'src/components/Section';
 import { CallToAction, ICallToAction } from '../CallToAction';
 import { Container } from '../Container';
-import { ILink, Link } from '../Link';
 
 export interface IActionBar {
   enabled: boolean;
@@ -26,17 +24,25 @@ export const ActionBar: React.FunctionComponent<IActionBar> = withSiteData(props
   return (
     <Section>
       <Container>
-        <div className={cn(className, 'flex items-center sm:flex-col sm:justify-center sm:items-between')}>
-          <div className="flex-row justify-center sm:text-center">
+        <div className={cn(className, 'flex items-center md:flex-wrap md:justify-center md:content-between')}>
+          <div className="justify-center md:text-center">
             {headline && <div className="flex-1 font-bold text-5xl mb-4">{headline}</div>}
 
             {text && <div className="flex-1 font-bold text-xl mb-4">{text}</div>}
           </div>
 
           {ctas && (
-            <div className="flex-1 flex justify-end sm:justify-center sm:items-between sm:flex-wrap">
+            <div className="flex flex-1 justify-end md:justify-center md:content-between md:flex-col">
               {ctas.map((cta, index) => (
-                <CallToAction key={index} className={index > 0 ? 'ml-4 sm:ml-0' : ''} {...cta} />
+                <CallToAction
+                  key={index}
+                  className={cn(
+                    className,
+                    index > 0 ? 'ml-4 md:ml-0' : '',
+                    'md:flex md:justify-center md:pt-4 md:w-full',
+                  )}
+                  {...cta}
+                />
               ))}
             </div>
           )}
